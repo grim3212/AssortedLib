27f024e60cdc43837b9c020f5a9aeea4e6bb0b7f
Dakota Pierone <dakotapierone@yahoo.com>
2023-04-08 15:58:02 -0700

    Reorg fabric events so load order does not matter

--- fabric/src/main/java/com/grim3212/assorted/lib/AssortedLibFabric.java
+++ fabric/src/main/java/com/grim3212/assorted/lib/AssortedLibFabric.java
@@ -1,33 +1,15 @@
 package com.grim3212.assorted.lib;
 
-import com.grim3212.assorted.lib.events.FabricLootTableModificationContext;
-import com.grim3212.assorted.lib.events.LootTableModifyEvent;
-import com.grim3212.assorted.lib.events.UseBlockEvent;
 import com.grim3212.assorted.lib.platform.FabricConfigHelper;
 import com.grim3212.assorted.lib.platform.Services;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.player.UseBlockCallback;
-import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.level.Level;
-import net.minecraft.world.phys.BlockHitResult;
 
 public class AssortedLibFabric implements ModInitializer {
 
 
     @Override
     public void onInitialize() {
-        Services.EVENTS.registerEventType(UseBlockEvent.class, () -> UseBlockCallback.EVENT.register((Player player, Level level, InteractionHand hand, BlockHitResult hitResult) -> {
-            final UseBlockEvent event = new UseBlockEvent(player, level, hand, hitResult);
-            Services.EVENTS.handleEvents(event);
-            return event.getInteractionResult();
-        }));
-
-        Services.EVENTS.registerEventType(LootTableModifyEvent.class, () -> LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
-            final LootTableModifyEvent event = new LootTableModifyEvent(lootManager, id, new FabricLootTableModificationContext(tableBuilder), source.isBuiltin());
-            Services.EVENTS.handleEvents(event);
-        }));
+        LibConstants.LOG.info(LibConstants.MOD_NAME + " starting up...");
 
         Services.CONDITIONS.init();
         Services.INGREDIENTS.register();
--- fabric/src/main/java/com/grim3212/assorted/lib/platform/FabricEventHelper.java
+++ fabric/src/main/java/com/grim3212/assorted/lib/platform/FabricEventHelper.java
@@ -2,8 +2,18 @@ package com.grim3212.assorted.lib.platform;
 
 import com.google.common.collect.ArrayListMultimap;
 import com.google.common.collect.Multimap;
+import com.grim3212.assorted.lib.events.FabricLootTableModificationContext;
 import com.grim3212.assorted.lib.events.GenericEvent;
+import com.grim3212.assorted.lib.events.LootTableModifyEvent;
+import com.grim3212.assorted.lib.events.UseBlockEvent;
 import com.grim3212.assorted.lib.platform.services.IEventHelper;
+import net.fabricmc.fabric.api.event.player.UseBlockCallback;
+import net.fabricmc.fabric.api.loot.v2.LootTableEvents;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.BlockHitResult;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -27,10 +37,36 @@ public class FabricEventHelper implements IEventHelper {
 
     @Override
     public void registerEvent(Class<? extends GenericEvent> eventType, Consumer<?> handler) {
+        if (!initialized) {
+            this.init();
+        }
+
         Runnable initializer = eventInits.remove(eventType);
         if (initializer != null) {
             initializer.run();
         }
         eventHandlers.put(eventType, handler);
+    }
+
+    private boolean initialized = false;
+
+    private void init() {
+        // Fabric doesn't guarantee load order so prime these instead of relying on Assorted Lib to load first
+        this.initialized = true;
+
+        Services.EVENTS.registerEventType(UseBlockEvent.class, () -> UseBlockCallback.EVENT.register((Player player, Level level, InteractionHand hand, BlockHitResult hitResult) -> {
+            final UseBlockEvent event = new UseBlockEvent(player, level, hand, hitResult);
+            Services.EVENTS.handleEvents(event);
+            if (event.isCanceled()) {
+                return InteractionResult.FAIL;
+            }
+
+            return event.getInteractionResult();
+        }));
+
+        Services.EVENTS.registerEventType(LootTableModifyEvent.class, () -> LootTableEvents.MODIFY.register((resourceManager, lootManager, id, tableBuilder, source) -> {
+            final LootTableModifyEvent event = new LootTableModifyEvent(lootManager, id, new FabricLootTableModificationContext(tableBuilder), source.isBuiltin());
+            Services.EVENTS.handleEvents(event);
+        }));
     }
 }
